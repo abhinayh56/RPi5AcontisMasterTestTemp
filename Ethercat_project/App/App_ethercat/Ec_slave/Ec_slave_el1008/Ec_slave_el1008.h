7677d7ccb336406760adaf7cf4fcca01c7f3c995
Bhanumati Seervi <bhanumatiseervi2@gmail.com>
2026-03-27 19:34:54 +0530

    sdfg

---
+++ Ethercat_project/App/App_ethercat/Ec_slave/Ec_slave_el1008/Ec_slave_el1008.h
@@ -0,0 +1,50 @@
+#ifndef EC_SLAVE_EL1008_H
+#define EC_SLAVE_EL1008_H
+
+#include "Ec_slave_base.h"
+
+namespace Ec_slave_el1008_data
+{
+	struct TxPdo
+	{
+		PdoVariable<0x6000, 1, bool> Channel_1;
+		PdoVariable<0x6010, 1, bool> Channel_2;
+		PdoVariable<0x6020, 1, bool> Channel_3;
+		PdoVariable<0x6030, 1, bool> Channel_4;
+		PdoVariable<0x6040, 1, bool> Channel_5;
+		PdoVariable<0x6050, 1, bool> Channel_6;
+		PdoVariable<0x6060, 1, bool> Channel_7;
+		PdoVariable<0x6070, 1, bool> Channel_8;
+	};
+}
+
+class Ec_slave_el1008 : public Ec_slave_base
+{
+public:
+	Ec_slave_el1008(uint16_t slaveAddr);
+
+	~Ec_slave_el1008();
+
+	virtual EC_T_DWORD registerTxPdos();
+
+	virtual EC_T_DWORD registerRxPdos();
+
+	virtual EC_T_DWORD transferTxPdo();
+
+	virtual EC_T_DWORD transferRxPdo();
+
+	virtual EC_T_DWORD processTxPdo();
+
+	virtual EC_T_DWORD processRxPdo();
+
+	virtual EC_T_DWORD publishData();
+
+	virtual EC_T_DWORD subscribeData();
+
+	virtual EC_T_DWORD mainProcess();
+
+protected:
+	Ec_slave_el1008_data::TxPdo m_TxPdo;
+};
+
+#endif // EC_SLAVE_EL1008_H
