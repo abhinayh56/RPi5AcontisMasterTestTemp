4ec4e0dc37d365aede5e661ef33742454f90bf6f
Bhanumati Seervi <bhanumatiseervi2@gmail.com>
2026-03-27 20:26:22 +0530

    asdfasdf

---
+++ Ethercat_project/App/App_ethercat/Ec_slave/Ec_slave_el1008/Ec_slave_el1008.cpp
@@ -0,0 +1,104 @@
+#include "Ec_slave_el1008.h"
+
+Ec_slave_el1008::Ec_slave_el1008(uint16_t slaveAddr) : Ec_slave_base(slaveAddr)
+{
+}
+
+Ec_slave_el1008::~Ec_slave_el1008()
+{
+}
+
+EC_T_DWORD Ec_slave_el1008::registerTxPdos()
+{
+	EC_T_DWORD dwRes = EC_E_NOERROR;
+
+	dwRes |= lookupInputPdoObject(m_SlaveAddr, m_TxPdo.Channel_1);
+	dwRes |= lookupInputPdoObject(m_SlaveAddr, m_TxPdo.Channel_2);
+	dwRes |= lookupInputPdoObject(m_SlaveAddr, m_TxPdo.Channel_3);
+	dwRes |= lookupInputPdoObject(m_SlaveAddr, m_TxPdo.Channel_4);
+	dwRes |= lookupInputPdoObject(m_SlaveAddr, m_TxPdo.Channel_5);
+	dwRes |= lookupInputPdoObject(m_SlaveAddr, m_TxPdo.Channel_6);
+	dwRes |= lookupInputPdoObject(m_SlaveAddr, m_TxPdo.Channel_7);
+	dwRes |= lookupInputPdoObject(m_SlaveAddr, m_TxPdo.Channel_8);
+
+	return dwRes;
+}
+
+EC_T_DWORD Ec_slave_el1008::registerRxPdos()
+{
+	EC_T_DWORD dwRes = EC_E_NOERROR;
+
+	return dwRes;
+}
+
+EC_T_DWORD Ec_slave_el1008::transferTxPdo()
+{
+	EC_T_DWORD dwRes = EC_E_NOERROR;
+
+	EC_T_BYTE* pBuffer = ecatGetProcessImageInputPtr();
+
+	transferInputPdoObject(m_TxPdo.Channel_1, pBuffer);
+	transferInputPdoObject(m_TxPdo.Channel_2, pBuffer);
+	transferInputPdoObject(m_TxPdo.Channel_3, pBuffer);
+	transferInputPdoObject(m_TxPdo.Channel_4, pBuffer);
+	transferInputPdoObject(m_TxPdo.Channel_5, pBuffer);
+	transferInputPdoObject(m_TxPdo.Channel_6, pBuffer);
+	transferInputPdoObject(m_TxPdo.Channel_7, pBuffer);
+	transferInputPdoObject(m_TxPdo.Channel_8, pBuffer);
+
+	return dwRes;
+}
+
+EC_T_DWORD Ec_slave_el1008::transferRxPdo()
+{
+	EC_T_DWORD dwRes = EC_E_NOERROR;
+
+	return dwRes;
+}
+
+EC_T_DWORD Ec_slave_el1008::processTxPdo()
+{
+	EC_T_DWORD dwRes = EC_E_NOERROR;
+
+	return dwRes;
+}
+
+EC_T_DWORD Ec_slave_el1008::processRxPdo()
+{
+	EC_T_DWORD dwRes = EC_E_NOERROR;
+
+	return dwRes;
+}
+
+EC_T_DWORD Ec_slave_el1008::publishData()
+{
+	EC_T_DWORD dwRes = EC_E_NOERROR;
+
+	return dwRes;
+}
+
+EC_T_DWORD Ec_slave_el1008::subscribeData()
+{
+	EC_T_DWORD dwRes = EC_E_NOERROR;
+
+	return dwRes;
+}
+
+EC_T_DWORD Ec_slave_el1008::mainProcess()
+{
+	EC_T_DWORD dwRes = EC_E_NOERROR;
+
+	std::cout <<
+	"SLAVE_ADDR: " << m_SlaveAddr << " | " <<
+	"Channel_1: " << m_TxPdo.Channel_1.value << ", "
+	"Channel_2: " << m_TxPdo.Channel_2.value << ", "
+	"Channel_3: " << m_TxPdo.Channel_3.value << ", "
+	"Channel_4: " << m_TxPdo.Channel_4.value << ", "
+	"Channel_5: " << m_TxPdo.Channel_5.value << ", "
+	"Channel_6: " << m_TxPdo.Channel_6.value << ", "
+	"Channel_7: " << m_TxPdo.Channel_7.value << ", "
+	"Channel_8: " << m_TxPdo.Channel_8.value
+	<< std::endl;
+
+	return dwRes;
+}
