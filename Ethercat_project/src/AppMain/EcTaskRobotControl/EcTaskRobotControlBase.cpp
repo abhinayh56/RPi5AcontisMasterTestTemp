52c2036bf356f8a5cf6b596c55b8bde45216a5e5
Abhinay Kumar <kumarabhinay53@gmail.com>
2026-04-05 03:12:45 +0530

    adsf

--- Ethercat_project/src/AppMain/EcTaskRobotControl/EcTaskRobotControlBase.cpp
+++ Ethercat_project/src/AppMain/EcTaskRobotControl/EcTaskRobotControlBase.cpp
@@ -1,19 +1,19 @@
-// #include "EcTaskRobotControlBase.h"
+#include "EcTaskRobotControlBase.h"
 
-// EcTaskRobotControlBase::EcTaskRobotControlBase(uint16_t ecTaskRobotControlId, const std::string &ecTaskRobotControlName) : m_ecTaskRobotControlId(ecTaskRobotControlId), m_ecTaskRobotControlName(ecTaskRobotControlName)
-// {
-// }
+EcTaskRobotControlBase::EcTaskRobotControlBase(uint16_t ecTaskRobotControlId, const std::string &ecTaskRobotControlName) : m_ecTaskRobotControlId(ecTaskRobotControlId), m_ecTaskRobotControlName(ecTaskRobotControlName)
+{
+}
 
-// EcTaskRobotControlBase::~EcTaskRobotControlBase()
-// {
-// }
+EcTaskRobotControlBase::~EcTaskRobotControlBase()
+{
+}
 
-// uint16_t EcTaskRobotControlBase::getTaskId()
-// {
-//     return m_ecTaskRobotControlId;
-// }
+uint16_t EcTaskRobotControlBase::getTaskId()
+{
+    return m_ecTaskRobotControlId;
+}
 
-// std::string &EcTaskRobotControlBase::getTaskName()
-// {
-//     return m_ecTaskRobotControlName;
-// }
+std::string &EcTaskRobotControlBase::getTaskName()
+{
+    return m_ecTaskRobotControlName;
+}
